59ba046291a203ee40d602374d55dedeba98077a
Lexkane <40840623+Lexkane@users.noreply.github.com>
2019-05-04 17:42:14 +0300

    Add files via upload

---
+++ SystemInformation/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+//Simple program printing System Information
+public class Main {
+	public static void main(String[] args) {
+    System.out.println("java.version: "+System.getProperty("java.version"));	
+	System.out.println("java.vm.name: "+System.getProperty("java.vm.name"));
+	System.out.println("java.home: : "+System.getProperty("java.home"));
+	System.out.println("os.version: "+System.getProperty("os.version"));
+	System.out.println("os.name: "+System.getProperty("os.name"));
+	
+		
+    }
+
+}
